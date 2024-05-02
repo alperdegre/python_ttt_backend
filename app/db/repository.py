d55d8b18b5d41396526609c6efd31e77b6266966
Alper Değre <alper.degre.dev@gmail.com>
2024-05-02 03:20:06 +0300

    add get_user_by_username repo func, fix create_user to not include password

--- app/db/repository.py
+++ app/db/repository.py
@@ -13,4 +13,15 @@ def create_user(user_req:CreateUserRequest,db:Session):
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
-    return db_user.__dict__
+
+    user_dict = db_user.__dict__
+    del user_dict['password']
+    del user_dict['created_at']
+    return user_dict
+
+def get_user_by_username(user_name:str, db:Session):
+    db_user = db.query(User).filter(User.username == user_name).first()
+    if db_user is None:
+        return None
+    else:
+        return db_user
--- app/tests/db/test_repository.py
+++ app/tests/db/test_repository.py
@@ -1,10 +1,24 @@
 
-from app.db.repository import create_user, CreateUserRequest
+from app.db.repository import create_user, CreateUserRequest, get_user_by_username
 
 
 def test_create_user(session):
     user = create_user(CreateUserRequest(username="alper", password="test"), session)
 
     assert user['username'] == "alper"
-    assert user['password'] == "test"
+    assert 'password' not in user
+    assert 'created_at' not in user
     
+def test_get_user_by_username(session):
+    user = get_user_by_username('test', session)
+
+    assert user is None
+
+    username = 'test_get_user'
+
+    create_user(CreateUserRequest(username=username, password="test"), session)
+
+    existing_user = get_user_by_username(username,session)
+
+    assert existing_user is not None
+    assert existing_user.username == username
