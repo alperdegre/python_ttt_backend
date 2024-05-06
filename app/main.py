14c446a3b4dbe511170863a813e1e0742c64c6c4
Alper Değre <alper.degre.dev@gmail.com>
2024-05-06 16:58:55 +0300

    rename router on main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.engine.engine import GameEngine
-from app.routers import auth, game
+from app.routers import auth, lobby
 
 app = FastAPI()
 
@@ -17,7 +17,7 @@ app.add_middleware(
 )
 
 app.include_router(auth.router)
-app.include_router(game.router)
+app.include_router(lobby.router)
 
 @app.get("/hello-world")
 async def root():
