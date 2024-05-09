b081f4eff89da1834f97cc4e73330d59f53b715e
Alper Değre <alper.degre.dev@gmail.com>
2024-05-09 03:14:36 +0300

    finalize engine and its tests

--- app/engine/engine.py
+++ app/engine/engine.py
@@ -1,27 +1,8 @@
-
 from typing import Dict, Optional
 from app.engine.game import Game
 from app.engine.lobby import Lobby
+from app.models import ListedLobby
 from app.utils import create_game_code, create_lobby_code
-
-
-"""
-Create lobby happens on HTTP. Gets a WS url with lobby code
-    - User sends back a ws response. gets s generic "Connection established if its succesful". Lobby screen is ws based.
-    - Join lobby means getting access to that lobby's ws url. Join lobby function has to check if current lobby is full or not. 
-    Return a HTTP response accordinglyor join the lobby.
-
-Lobby features
-    - Disconnect means losing the WS connection.
-    - When start game gets pressed.It sends an event through frontend. Which should disconnect the ws connection. 
-        - Connect to a new ws url where only 2 users for the game can join it.
-        - Games have their own while true loop inside. Users always append game id + user id. That is how game proceeds.
-
-Game features
-    - Every turn keep track of whose turn it is. When you recieve an event from whose turn it is. Update turn variable and add the move after. This should solve duplicate requests.
-
-"""
-
 
 
 class GameEngine():
@@ -40,13 +21,16 @@ class GameEngine():
             return self.lobbies[lobby_code]
         else:
             return None
+        
+    def get_lobbies(self):
+        return [ListedLobby(code=lobby.code, owner=lobby.owner, players=lobby.users.values()) for lobby in self.lobbies.values()]
 
     def close_lobby(self, lobby_code:str):
         if lobby_code in self.lobbies:
             del self.lobbies[lobby_code]
         else:
             return None
-    
+        
     def get_unique_lobby_code(self):
         code = create_lobby_code()
         while code in self.lobbies:
@@ -68,12 +52,23 @@ class GameEngine():
             return
         
         game_code = self.get_unique_game_code()
-        game = Game(websockets=lobby.websockets, users=lobby.users)
+        game = Game(users={**lobby.users})
 
         self.games[game_code] = game
         return game_code
-
-     
+    
+    def get_game(self, game_code:str) -> Optional[Game]:
+        if game_code in self.games:
+            return self.games[game_code]
+        else:
+            return None
+        
+    def close_game(self, game_code:str):
+        if game_code in self.games:
+            del self.games[game_code]
+        else:
+            return None
+        
 game_engine = GameEngine()
 
 def get_engine():
--- app/tests/engine/test_engine.py
+++ app/tests/engine/test_engine.py
@@ -5,6 +5,7 @@ from unittest.mock import patch
 import pytest
 
 from app.engine.engine import GameEngine
+from app.engine.game import Game, GameStatusEnum
 from app.engine.lobby import Lobby
 from app.models import LobbyUser
 
@@ -116,3 +117,91 @@ async def test_start_lobby_success(lobby_user, mock_websocket):
         assert result is "GAME_CODE"
         mock_game_code.assert_called_once()
         assert "GAME_CODE" in engine.games
+
+def test_get_game_success(lobby_user):
+    engine = GameEngine()
+    engine.games["TEST"] = Game(users={'USER_ID':lobby_user})
+
+    game = engine.get_game("TEST")
+
+    assert "USER_ID" in game.users
+    assert game.users['USER_ID'] is lobby_user
+    assert game.status is GameStatusEnum.FORMING
+    
+def test_get_game_fail():
+    engine = GameEngine()
+
+    game = engine.get_game("TEST_NON_EXISTENT")
+    assert game is None
+
+@pytest.mark.anyio
+async def test_get_lobbies(lobby_user, mock_websocket):
+    engine = GameEngine()
+    with patch('app.engine.engine.GameEngine.get_unique_lobby_code', return_value="CODE"), patch('app.engine.engine.GameEngine.get_unique_game_code', return_value="GAME_CODE"):
+        engine.create_lobby("TEST_OWNER")
+        lobby = engine.get_lobby("CODE")
+
+        await lobby.join(lobby_user, mock_websocket)
+
+        lobbies = engine.get_lobbies()
+
+        assert lobbies[0].code is lobby.code
+        assert lobbies[0].owner is lobby.owner
+        assert lobbies[0].players[0] is lobby_user
+
+@pytest.mark.anyio
+async def test_get_game_success(lobby_user, mock_websocket):
+    engine = GameEngine()
+    with patch('app.engine.engine.GameEngine.get_unique_lobby_code', return_value="CODE"), patch('app.engine.engine.GameEngine.get_unique_game_code', return_value="GAME_CODE"):
+        engine.create_lobby("TEST_OWNER")
+
+        lobby = engine.get_lobby("CODE")
+
+        await lobby.join(lobby_user, mock_websocket)
+        mock_user_two = LobbyUser(id="user_2", username="user_two")
+        await lobby.join(mock_user_two, mock_websocket)
+
+        result = engine.start_lobby("CODE")
+
+        get_game = engine.get_game(result)
+
+        assert get_game is engine.games[result]
+        assert len(get_game.users) is 2
+        assert lobby_user in get_game.users.values()
+        assert mock_user_two in get_game.users.values()
+
+def test_get_game_failure():
+    engine = GameEngine()
+
+    game_doesnt_exist = engine.get_game("NON_EXISTENT")
+
+    assert game_doesnt_exist is None
+
+
+@pytest.mark.anyio
+async def test_close_game_success(lobby_user, mock_websocket):
+    engine = GameEngine()
+    with patch('app.engine.engine.GameEngine.get_unique_lobby_code', return_value="CODE"), patch('app.engine.engine.GameEngine.get_unique_game_code', return_value="GAME_CODE"):
+        engine.create_lobby("TEST_OWNER")
+        lobby = engine.get_lobby("CODE")
+        await lobby.join(lobby_user, mock_websocket)
+        mock_user_two = LobbyUser(id="user_2", username="user_two")
+        await lobby.join(mock_user_two, mock_websocket)
+
+        result = engine.start_lobby("CODE")
+        get_game = engine.get_game(result)
+
+        assert get_game is not None
+
+        engine.close_game(result)
+
+        assert len(engine.games) is 0
+
+def test_close_game_failure():
+    engine = GameEngine()
+
+    result = engine.close_game("NON_EXISTENT")
+
+    assert result is None
+
+    
